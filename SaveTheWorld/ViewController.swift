9d3f1b1af232a19c73a2877de58c1909b730d2da
wilson sonkamble <wilsonsonkamble@wilsons-macbook-pro.local>
2017-05-06 01:23:21 +0530

    Initial Commit

---
+++ SaveTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheWorld
+//
+//  Created by wilson sonkamble on 5/6/17.
+//  Copyright © 2017 Trinity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
